5b161a0584b320510debe669f9172e0647b36549
Mehmet Başgöze <mhmtbsgz34@gmail.com>
2024-03-30 22:37:10 +0300

    First commit

---
+++ App.js
@@ -0,0 +1,49 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+//import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from 'react';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    regular: require("./assets/fonts/Poppins-Regular.ttf"),
+    light: require("./assets/fonts/Poppins-Light.ttf"),
+    bold: require("./assets/fonts/Poppins-Bold.ttf"),
+    medium: require("./assets/fonts/Poppins-Medium.ttf"),
+    extrabold: require("./assets/fonts/Poppins-ExtraBold.ttf"),
+    semibold: require("./assets/fonts/Poppins-SemiBold.ttf"),
+  })
+
+  const onLayoutRootView = useCallback(async() => {
+
+    if(fontsLoaded){
+      await SplashScreen.hideAsync();
+    }
+  },[fontsLoaded] );
+
+  if(!fontsLoaded){
+    return null;
+  }
+
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textStyles}>Merhaba proje kodlarının ilk deneme satırı</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyles: {
+    fontFamily: "extrabold",
+    fontSize: 20,
+  }
+});
