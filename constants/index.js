26619ea04725a9d02f6db396a9464e508558676e
Mehmet Başgöze <mhmtbsgz34@gmail.com>
2024-04-23 17:22:47 +0300

    Görünüm klasörü düzeltildi

--- constants/index.js
+++ constants/index.js
@@ -1,10 +1,26 @@
 export const COLORS = {
-    primary: '#FF5733',
-    secondary: '#3366FF',
-    accent: '#FFC300',
-    gray2: '#CCCCCC'
-  };
+  primary: "2A4050",
+  secondary: "#DDf0ff",
+  tertiary: "#FF7754",
+
+  gray: "#83829A",
+  gray2: "#C1C0C0",
+
+  offwhite: "#F3F4F8",
+  white: "#FFFFFF",
+  black: "#000000",
+  red: "#e81e4d",
+  green: "#00C135",
+  lightWhite: "#FAFAFC"
+};
 
 export const SIZES = {
-  
+  xSmall: 10,
+  small: 12,
+  medium: 16,
+  large: 20,
+  xLarge: 24,
+  xxLarge: 44,
+  height: 15,
+  width: 10
 }
