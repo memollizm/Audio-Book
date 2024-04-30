f9595b1680066baf484bd1341688c26828b37498
Mehmet Başgöze <mhmtbsgz34@gmail.com>
2024-04-30 23:27:12 +0300

    Ürün detay sayfasına navigasyon eklendi

--- screens/index.js
+++ screens/index.js
@@ -2,9 +2,12 @@ import Search from "./Search";
 import Home from "./Home";
 import Profile from "./Profile";
 import Cart from "./Cart";
+import Details from "./Details";
 
 export {
     Home,
     Search,
-    Profile,Cart
+    Profile,
+    Cart,
+    Details
 }
